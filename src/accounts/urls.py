98ee2fa2cdf7fb6f0665c43bb7d6ecb6d52ffc3f
SleepNoMore <nninkovic.py@gmail.com>
2020-12-24 01:34:34 +0100

    Finished task 3

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import create_user_form, home_view, UserListView, UserDetailView
 
@@ -14,4 +14,5 @@ urlpatterns = [
     path("users/", UserListView.as_view(), name="users"),
     path("users/details/<int:pk>/", UserDetailView.as_view(), name="details"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("hijack/", include("hijack.urls", namespace="hijack")),
 ]
--- src/job_task_core/settings.py
+++ src/job_task_core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
     "crispy_forms",
+    "hijack",
+    "compat",
 ]
 
 MIDDLEWARE = [
@@ -120,11 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 
 AUTH_USER_MODEL = "accounts.User"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home"
